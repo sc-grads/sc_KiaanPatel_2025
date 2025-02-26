e1ff636c952b98b6080c728b2cf719481ec7573e
KiaanPatel <kiaan.patel@sambeconsulting.com>
2025-02-26 14:06:20 +0200

    Adding Git Actions SQL file

---
+++ DatabaseAdministration/SQLStatements/GitActionsDatabaseTestQuery.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE MyDatabase;
+GO
+USE MyDatabase;
+
+CREATE TABLE User (
+
+	User_ID INT PRIMARY KEY IDENTITY(1,1),
+	User_Name VARCHAR(50) NOT NULL,
+	User_Surname VARCHAR(50) NOT NULL,
+	User_Email VARCHAR(100) UNIQUE NOT NULL,
+	User_PhoneNumber VARCHAR(10) UNIQUE,
+	User_Address VARCHAR(100)
+);
+GO
