e6832fbe2d38b7c45d84394bfa4d75da7e948c28
KiaanPatel <kiaan.patel@sambeconsulting.com>
2025-02-27 11:05:54 +0200

    Update InsertQuery.sql

--- DatabaseAdministration/SQLStatements/InsertQuery.sql
+++ DatabaseAdministration/SQLStatements/InsertQuery.sql
@@ -1,2 +1,4 @@
-INSERT INTO dbo.Customer (CustomerName, CustomerSurname, CustomerEmail, CustomerCellNumber, CustomerDateOfBirth) 
-VALUES ('James', 'Doe', 'jamesdoe@gmail.com', 0641235697, '1978-02-15')
+INSERT INTO dbo.customer 
+    (customername, customersurname, customeremail, customercellnumber, customerdateofbirth) 
+VALUES 
+    ('James', 'Doe', 'jamesdoe@gmail.com', 0641235697, '1978-02-15');
