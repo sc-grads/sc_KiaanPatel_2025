061041660acdab70cebc9cb6537dbd3072449b4a
KiaanPatel <kiaan.patel@sambeconsulting.com>
2025-03-10 22:25:25 +0200

    Adding Insert Query for Udemy course

---
+++ DatabaseAdministration/SQLStatements/InsertQueryUdemy.sql
@@ -0,0 +1,10 @@
+INSERT INTO [dbo].[PersonalInfo]
+           ([firstName]
+           ,[lastName]
+           ,[dob])
+     VALUES
+           ('Kiaan', 'Patel', '2003-11-28')
+GO
+
+SELECT * FROM PersonalInfo
+GO
