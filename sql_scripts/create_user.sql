bda20b4bfcb9526ec055122b6b73873bfc097dc8
KiaanPatel <kiaan.patel@sambeconsulting.com>
2025-03-25 14:11:31 +0200

    Create create_user.sql

---
+++ sql_scripts/create_user.sql
@@ -0,0 +1,18 @@
+IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'Auto_user')
+BEGIN
+    CREATE LOGIN Auto_user WITH PASSWORD = 'Password123';
+END
+GO
+USE AutoTest;
+GO
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'Auto_user')
+BEGIN
+    CREATE USER Auto_user FOR LOGIN Auto_user;
+END
+GO
+ALTER ROLE db_owner ADD MEMBER Auto_user;
+GO
+USE master;
+GO
+ALTER SERVER ROLE sysadmin ADD MEMBER Auto_user;
+GO
