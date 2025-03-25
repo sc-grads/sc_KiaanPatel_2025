9998a4bf673a2b33f2b191e11369f729d91e1a15
KiaanPatel <kiaan.patel@sambeconsulting.com>
2025-03-25 14:12:28 +0200

    Create insert_data.sql

---
+++ sql_scripts/insert_data.sql
@@ -0,0 +1,6 @@
+USE AutoTest;
+GO
+EXEC [dbo].[InsertUser] 'John', 'Doe', 'john.doe@example.com';
+EXEC [dbo].[InsertUser] 'Jane', 'Smith', 'jane.smith@example.com';
+EXEC [dbo].[InsertUser] 'Alice', 'Johnson', 'alice.johnson@example.com';
+GO
