fb7d5d339bdf1b9717c31feae28979e4a44d3c92
KiaanPatel <kiaan.patel@sambeconsulting.com>
2025-06-19 09:55:12 +0200

    Update CreateAgentJob.sql
    Signed-off-by: KiaanPatel <kiaan.patel@sambeconsulting.com>

--- HandsOnProject/Timesheet/CreateAgentJob.sql
+++ HandsOnProject/Timesheet/CreateAgentJob.sql
@@ -1,47 +1,44 @@
-USE msdb;
+USE [msdb]
 GO
-
--- Check if the job already exists and delete it to avoid conflicts
-IF EXISTS (SELECT job_id FROM msdb.dbo.sysjobs WHERE name = N'RunTimesheetEntryEveryMinute')
-BEGIN
-    EXEC msdb.dbo.sp_delete_job @job_name = N'RunTimesheetEntryEveryMinute', @delete_unused_schedule = 1;
-END
+DECLARE @jobId BINARY(16)
+EXEC  msdb.dbo.sp_add_job @job_name=N'Run_Timesheet_SSIS_Job', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
+		@notify_level_page=2, 
+		@delete_level=0, 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'Sambe202507\Kiaan Patel', @job_id = @jobId OUTPUT
+select @jobId
 GO
-
--- Create the SQL Server Agent job
-DECLARE @jobId BINARY(16);
-EXEC msdb.dbo.sp_add_job
-    @job_name = N'RunTimesheetEntryEveryMinute',
-    @enabled = 1,
-    @description = N'Job to execute TimesheetEntry.dtsx SSIS package every minute',
-    @owner_login_name = N'sa', -- Replace with your login if not 'sa'
-    @job_id = @jobId OUTPUT;
-
--- Add a job step to execute the SSIS package
-EXEC msdb.dbo.sp_add_jobstep
-    @job_id = @jobId,
-    @step_name = N'Execute TimesheetEntry SSIS Package',
-    @step_id = 1,
-    @subsystem = N'SSIS',
-    @command = N'/ISSERVER "\"\SSISDB\SSISProject\SSISProjectRun\TimesheetEntry.dtsx\"" /SERVER "\".\""',
-    @on_success_action = 1, -- Quit with success
-    @on_fail_action = 2; -- Quit with failure
-
--- Add a schedule to run every minute
-EXEC msdb.dbo.sp_add_jobschedule
-    @job_id = @jobId,
-    @name = N'EveryMinuteSchedule_Timesheet',
-    @enabled = 1,
-    @freq_type = 4, -- Daily
-    @freq_interval = 1,
-    @freq_subday_type = 4, -- Minute
-    @freq_subday_interval = 1, -- Every 1 minute
-    @active_start_date = 20250618,
-    @active_end_date = 99991231,
-    @active_start_time = 000000; -- Start at midnight
-
--- Assign the job to the target server
-EXEC msdb.dbo.sp_add_jobserver
-    @job_id = @jobId,
-    @server_name = N'(local)'; -- Adjust if not local
+EXEC msdb.dbo.sp_add_jobserver @job_name=N'Run_Timesheet_SSIS_Job', @server_name = N'SAMBE202507'
+GO
+USE [msdb]
+GO
+EXEC msdb.dbo.sp_add_jobstep @job_name=N'Run_Timesheet_SSIS_Job', @step_name=N'Step1', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_fail_action=2, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'SSIS', 
+		@command=N'/ISSERVER "\"\SSISDB\SSISProject\SSISProjectRun\TimesheetEntry.dtsx\"" /SERVER "\".\"" /Par "\"$ServerOption::LOGGING_LEVEL(Int16)\"";1 /Par "\"$ServerOption::SYNCHRONIZED(Boolean)\"";True /CALLERINFO SQLAGENT /REPORTING E', 
+		@database_name=N'master', 
+		@flags=0
+GO
+USE [msdb]
+GO
+EXEC msdb.dbo.sp_update_job @job_name=N'Run_Timesheet_SSIS_Job', 
+		@enabled=1, 
+		@start_step_id=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
+		@notify_level_page=2, 
+		@delete_level=0, 
+		@description=N'', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'Sambe202507\Kiaan Patel', 
+		@notify_email_operator_name=N'', 
+		@notify_page_operator_name=N''
 GO
