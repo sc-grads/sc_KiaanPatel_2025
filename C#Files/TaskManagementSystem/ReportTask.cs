da6f9b73bdfe89bf2c7243e1822192d1ec33f256
Kiaan Patel <kiaan.patel@sambeconsulting.com>
2025-09-04 10:13:14 +0200

    added git ignore changes

---
+++ C#Files/TaskManagementSystem/ReportTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem
+{
+    internal class ReportTask
+    {
+        public int MyProperty { get; set; }
+        public string ReportName { get; set; }
+
+        public string Perform()
+        {
+            return $"Report {ReportName} generated successfully";
+        }
+
+    }
+
+}
