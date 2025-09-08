1e99cb78bf71f21f94658f8ca70dd1b4ba3b11c1
Kiaan Patel <kiaan.patel@sambeconsulting.com>
2025-09-08 10:30:07 +0200

    completed vid 383

--- C#Files/Calculator/UnitTest1.cs
+++ C#Files/Calculator/UnitTest1.cs
@@ -1,11 +1,20 @@
-namespace Calculator
+using Domain;
+
+namespace CalculatorTest
 {
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
+            var calculator = new Calculator();
+            if (calculator.Sum(2, 2) != 4)
+            {
+                throw new Exception();
+            }
 
         }
+
+        
     }
 }
