43c1cc97076d6aafdc2597265439dd284c182add
Kiaan Patel <kiaan.patel@sambeconsulting.com>
2025-09-09 10:55:49 +0200

    updated work, doing flgith program

--- C#Files/Calculator/UnitTest1.cs
+++ C#Files/Calculator/UnitTest1.cs
@@ -8,7 +8,6 @@ namespace CalculatorTest
         public void Test1()
         {
             var calculator = new Calculator();
-            if (calculator.Sum(2, 2) != 4)
             {
                 throw new Exception();
             }
